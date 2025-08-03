5fbaaea1dcabc1629902e34fba2f7c8d294841a0
CharlesShango <camoah030@st.ug.edu.gh>
2025-08-03 09:19:59 +0000

    Add files via upload

---
+++ Ticket Price/Ticket Price/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter your age: ");
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int age))
+        {
+            double ticketPrice;
+
+            if (age <= 12 || age >= 65)
+                ticketPrice = 7.00;
+            else
+                ticketPrice = 10.00;
+
+            Console.WriteLine($"Your ticket price is: GHC{ticketPrice}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid age.");
+        }
+
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+    }
+}
